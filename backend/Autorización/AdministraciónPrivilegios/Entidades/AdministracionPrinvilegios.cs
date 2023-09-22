6df874b32e7ff4331762e2da40f0ef507ac42254
MichaelVeliz01 <141230537+MichaelVeliz01@users.noreply.github.com>
2023-09-22 17:48:22 -0400

    administracion y rol

--- backend/Autorización/AdministraciónPrivilegios/Entidades/AdministracionPrinvilegios.cs
+++
@@ -1,9 +0,0 @@
-namespace backend.Autorizacion.AsignacionPrivilegios.entidades
-{
-    public class AsignacionPrivilegios
-    {
-        public int ID { get; set; }
-        public int ID_Rol { get; set; }
-        public int ID_Funcionalidad { get; set; }
-    }
-}

0fb8ee05a66bfdf0a52a75f443aec1d838558d04
Maicol Erick Arteaga Guzman <88513626+Erick0711@users.noreply.github.com>
2023-09-25 18:32:08 -0400

    Se crea una nueva clase common, para heredarlo en todas las clases

--- frontend/frontend-upc/src/app/entidades/categoria-producto.ts
+++ frontend/frontend-upc/src/app/entidades/categoria-producto.ts
@@ -1,4 +1,6 @@
-export class CategoriaProducto{
+import { common } from "./common";
+
+export class CategoriaProducto extends common{
   public id: number = 0;  
   public nombre: string = "";
 }
--- frontend/frontend-upc/src/app/entidades/detalle-carrito.ts
+++ frontend/frontend-upc/src/app/entidades/detalle-carrito.ts
@@ -1,4 +1,7 @@
-export class DetalleCarrito{
+import { common } from "./common";
+
+
+export class DetalleCarrito extends common{
     public id: number = 0;  
     public cantidad: number = 1;
     public idProducto: number = 1;
